c555e57e59b46dc708cc9416184b7cfbc9d4aa84
Junhan <wym950805@gmail.com>
2024-09-17 17:22:23 +0800

    complete random network distillation

--- hw5/cs285/agents/rnd_agent.py
+++ hw5/cs285/agents/rnd_agent.py
@@ -6,6 +6,7 @@ from typing import Callable, List, Tuple
 
 from cs285.agents.dqn_agent import DQNAgent
 import cs285.infrastructure.pytorch_util as ptu
+from cs285.infrastructure.running_state import ZFilter
 
 def init_network(model):
     if isinstance(model, nn.Linear):
@@ -40,13 +41,16 @@ class RNDAgent(DQNAgent):
         self.rnd_optimizer = make_rnd_network_optimizer(
             self.rnd_net.parameters()
         )
+        self.rnd_normalizer = ZFilter((128,))
 
     def update_rnd(self, obs: torch.Tensor) -> torch.Tensor:
         """
         Update the RND network using the observations.
         """
+        prediction = self.rnd_net(obs)
+        target = self.rnd_target_net(obs)
         # TODO(student): update the RND network
-        loss = ...
+        loss = torch.square(prediction - target).mean()
 
         self.rnd_optimizer.zero_grad()
         loss.backward()
@@ -65,9 +69,13 @@ class RNDAgent(DQNAgent):
     ):
         with torch.no_grad():
             # TODO(student): Compute RND bonus for batch and modify rewards
-            rnd_error = ...
-            assert rnd_error.shape == rewards.shape
-            rewards = ...
+            prediction = self.rnd_net(next_observations)
+            target = self.rnd_target_net(next_observations) #  shape = (batch_size, rnd_dim)
+            rnd_error = torch.square(prediction - target).mean(dim=-1) # rnd_error大，说明这个next_observation没见过，更应去探索这个next_observation，也就是说更应去采用会产生这个next_observation的action
+            # rnd_error = (rnd_error - rnd_error.mean()) / (rnd_error.std() + 1e-8)
+            rnd_error = self.rnd_normalizer(rnd_error)
+            assert rnd_error.shape == rewards.shape, print(f'run_error.shape={rnd_error.shape}, rewards.shape={rewards.shape}')
+            rewards = rewards + self.rnd_weight * rnd_error
 
         metrics = super().update(observations, actions, rewards, next_observations, dones, step)
 
@@ -98,11 +106,11 @@ class RNDAgent(DQNAgent):
             xx, yy = torch.meshgrid(x, y)
 
             inputs = ptu.from_numpy(np.stack([xx.flatten(), yy.flatten()], axis=1))
-            targets = self.rnd_target_net(inputs)
+            targets = self.rnd_target_net(inputs) # shape = (10000,5)
             predictions = self.rnd_net(inputs)
 
-            errors = torch.norm(predictions - targets, dim=-1)
-            errors = torch.reshape(errors, xx.shape)
+            errors = torch.norm(predictions - targets, dim=-1) # shape = (10000,)
+            errors = torch.reshape(errors, xx.shape) # shape = (100,100)
 
             # Log scale, aligned with normal axes
             from matplotlib import cm
